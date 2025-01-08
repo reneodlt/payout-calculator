6b7f171fbfb37ebf6c582ec60f7b4323c8873af4
Rich Green <rich@reneodlt.com>
2025-01-08 21:56:29 +0000

    looks a bit better

---
+++ google.js
@@ -0,0 +1,14 @@
+// gtag.js
+(function() {
+    // Load the Google Analytics library
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-KJJXM8R82Q";
+    document.head.appendChild(script);
+
+    // Initialize Google Analytics
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-KJJXM8R82Q');
+})();
